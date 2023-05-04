9eac5af5cd9dff5d5ae0efd86eb3ff92d3bb3469
joaopedpaula <joao.paula@sptech.school>
2023-05-04 17:56:00 -0300

    Aula 04 - imagem de fundo / modo de edição

--- music-box/src/components/CardMusica.jsx
+++ music-box/src/components/CardMusica.jsx
@@ -1,23 +1,47 @@
 import editIcon from "../assets/imagens/edit-icon.png"
 import deleteIcon from "../assets/imagens/delete-icon.png"
+import imgCapaPadrao from "../assets/imagens/capa.png" // importando recurso de imagem para a capa padrão 
+
+import { useState } from "react";
 
 function CardMusica(props) {
 
-  // exibindo objeto de "musica" recebido como props
-  console.log('Objeto de "música" recebido como props: ', props.musica);
+  const [estaEditando, setEstaEditando] = useState(false); // estado para verificar se o usuário está editando o card de música
+
+  /*
+    Utilizando operador ternário para definir o valor inicial da imagem de fundo
+    O operador ternário funciona como um "if-else" de uma linha só: 
+        
+        condicao  ?  saida_se_true :  saida_se_false
+    
+    Abaixo, estamos criando uma constante "estilo", que é um objeto de configuração de estilo CSS, com a propriedade "backgroundImage".
+    Utilizando esse objeto, podemos alterar a imagem de fundo da div da classe "card-music".
+    E como imagem, estamos utilizando um operador ternário, onde como condição, usamos o "props.musica.imagem":
+        props.musica.imagem ? props.musica.imagem : imgCapaPadrao
+    
+    Assim, validamos se "props.musica.imagem" está vazio, onde:
+        Caso contenha algum valor armazenado, retornamos o próprio "props.musica.imagem" como valor.
+        Caso esteja vazio, retornamos o valor do recurso "imgCapaPadrao".
+  */
+  const estilo = {
+    backgroundImage: `url(${
+      props.musica.imagem ? props.musica.imagem : imgCapaPadrao
+    })`
+  }
 
   return (
-    <div className="card-music">
+    <div style={estilo} className="card-music"> {/* aplicando estilização com a const "estilo" para alterar imagem de fundo */}
       <div className="icons">
         {/* utilizando recursos de imagem como fonte da tag img */}
-        <img src={editIcon} alt="" />
+        <img onClick={() => setEstaEditando(!estaEditando)} src={editIcon} alt="" /> {/* ao clicar na imagem de lápis, habilita/desabilita o modo de edição */}
         <img src={deleteIcon} alt="" />
       </div>
       <div className="info-music">
         <p>
           <strong className="card-title">música: </strong>
           <input
-            className="input-music-enable"
+            disabled={estaEditando === false} // desabilitado se o modo de edição estiver como "false" 
+            className={estaEditando ? 'input-music-enable' : 'input-music-disabled'} // desabilitado se o modo de edição estiver como "false"
             type="text"
             defaultValue={props.musica.titulo} // utilizando atributo "título" do objeto "musica" recebido como props, para exibir como valor padrão da input
           />
@@ -25,7 +49,8 @@ function CardMusica(props) {
         <p>
           <strong className="card-title">artista: </strong>
           <input
-            className="input-music-enable"
+            disabled={estaEditando === false} // desabilitado se o modo de edição estiver como "false"
+            className={estaEditando ? 'input-music-enable' : 'input-music-disabled'} // desabilitado se o modo de edição estiver como "false"
             type="text"
             defaultValue={props.musica.artista} // utilizando atributo "artista" do objeto "musica" recebido como props, para exibir como valor padrão da input
           />
@@ -33,7 +58,8 @@ function CardMusica(props) {
         <p>
           <strong className="card-title">gênero: </strong>
           <input
-            className="input-music-enable"
+            disabled={estaEditando === false} // desabilitado se o modo de edição estiver como "false"
+            className={estaEditando ? 'input-music-enable' : 'input-music-disabled'} // desabilitado se o modo de edição estiver como "false"
             type="text"
             defaultValue={props.musica.genero} // utilizando atributo "genero" do objeto "musica" recebido como props, para exibir como valor padrão da input
           />
@@ -41,12 +67,16 @@ function CardMusica(props) {
         <p>
           <strong className="card-title">ano: </strong>
           <input
-            className="input-music-enable"
+            disabled={estaEditando === false} // desabilitado se o modo de edição estiver como "false"
+            className={estaEditando ? 'input-music-enable' : 'input-music-disabled'} // desabilitado se o modo de edição estiver como "false"
             type="text"
             defaultValue={props.musica.ano} // utilizando atributo "ano" do objeto "musica" recebido como props, para exibir como valor padrão da input
           />
         </p>
-        <button className="btn-salvar-enable">Salvar</button>
+        {/* ao clicar no botão, altera o estado "estaEditando" para false */}
+        <button onClick={() => setEstaEditando(false)} className={estaEditando ? 'btn-salvar-enable' : 'btn-salvar-disabled'}>
+          Salvar
+        </button>
       </div>
     </div>
   );
